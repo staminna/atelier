09ad0317c3d5db1c46335fbf90cf9620e2d86eab
Jorge Nunes <stamina.nunes@gmail.com>
2019-06-28 23:14:21 +0100

    Responsive for desktop, but pictures still need work

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Slider from "react-slick";
 
-export default class SimpleSlider extends React.Component {
+export default class Responsive extends Component {
   render() {
     var settings = {
       dots: true,
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
     return (
-      <Slider {...settings}>
-        <div>
-          <img alt="detalhe loja" className="fotos" src="fotos/1_detalhe_loja.jpg"/>
-        </div>
-        <div>
-          <img alt="telhado" className="fotos" src="fotos/2_telhado_editado.jpg"/>
-        </div>
-        <div>
-          <img alt="Santa luzia" className="fotos" src="fotos/3_santa_lusia.jpg"/>
-        </div>
-        <div>
-          <img alt="O Tacho" className="fotos" src="fotos/4_o_tacho.jpg"/>
-        </div>
-        <div>
-          <img alt="two living" className="fotos" src="fotos/6_two_living.jpg"/>
-        </div>
-        <div>
-          <img alt="Figueira" className="fotos" src="fotos/7_figueira_editado.jpg"/>
-        </div>
-      </Slider>
+      <div>
+        <h2> Modo(s). </h2>
+        <Slider {...settings}>
+          <div>
+            <img alt="detalhe loja" id="fotos" src="fotos/1_detalhe_loja.jpg"/>
+          </div>
+          <div>
+            <img alt="telhado" id="fotos" src="fotos/2_telhado_editado.jpg"/>
+          </div>
+          <div>
+            <img alt="Santa luzia" id="fotos" src="fotos/3_santa_lusia.jpg"/>
+          </div>
+          <div>
+            <img alt="O Tacho" id="fotos" src="fotos/4_o_tacho.jpg"/>
+          </div>
+          <div>
+            <img alt="two living" id="fotos" src="fotos/6_two_living.jpg"/>
+          </div>
+          <div>
+            <img alt="Figueira" id="fotos" src="fotos/7_figueira_editado.jpg"/>
+          </div>
+        </Slider>
+      </div>
     );
   }
 }
